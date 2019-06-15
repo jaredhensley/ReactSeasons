af4b01772f055758f14d42f2eb044ff0477c5250
Jared Hensley <jahensle85@me.com>
2019-06-14 19:19:37 -0500

    functional component to class component refact example

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    return <div>hi there!</div>
+// const App = () => {
+//     window.navigator.geolocation.getCurrentPosition(
+//         (position) => console.log(position),
+//         (err) => console.log(err)
+//     );
+//     return <div>lattitude: </div>
+// }
+
+class App extends React.Component {
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (err) => console.log(err)
+        );
+
+        return <div>lattitude: </div>
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector("#root"));
